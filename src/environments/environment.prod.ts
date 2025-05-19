68cd557024bf701a6ed1db3557bb7451db284a3f
draftfile203 <luka.xelaia123@gmail.com>
2025-05-19 15:18:33 +0400

    Add production environment config

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+   apiKey: "AIzaSyBwl-R_dXTdQPMoB6H53WrBEuDKvZON5T8",
+  authDomain: "agent-673a4.firebaseapp.com",
+  databaseURL: "https://agent-673a4-default-rtdb.firebaseio.com",
+  projectId: "agent-673a4",
+  storageBucket: "agent-673a4.firebasestorage.app",
+  messagingSenderId: "510561542111",
+  appId: "1:510561542111:web:29501ffe3fea98c2fb6e6a"
+  }
+};
